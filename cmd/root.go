0bdf724bbd4d3b382679428da279eb2572d23796
chjiahao123@163.com <Qqasdfghjkl123456789!@#>
2022-11-16 18:44:47 +0800

    添加编译时git版本信息打印

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
+	"calf-restful-api/version"
 	"fmt"
 	"github.com/spf13/cobra"
 )
 
 var (
-	version bool
+	vers bool
 )
 
 var RootCmd = &cobra.Command{
@@ -14,8 +15,9 @@ var RootCmd = &cobra.Command{
 	Long:  "restful-api-cli 后端API1",
 	Short: "restful-api-cli 后端API2",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if version {
+		if vers {
 			fmt.Println("restful-api-cli v1.0.0")
+			fmt.Println(version.FullVersion())
 		}
 		return nil
 	},
@@ -23,5 +25,5 @@ var RootCmd = &cobra.Command{
 
 func init() {
 	f := RootCmd.PersistentFlags()
-	f.BoolVarP(&version, "version", "v", false, "restful-api 版本信息")
+	f.BoolVarP(&vers, "version", "v", false, "restful-api 版本信息")
 }
